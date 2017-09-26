4031bb3dd51abfe47ebd22e5a35847038e5e7c51
Roman Sachse <rsachse@a-s-d.de>
2017-09-26 15:22:32 +0200

    EventStore eingebaut

---
+++ src/Infrastructure/EventStore.fs
@@ -0,0 +1,68 @@
+module Infrastructure.EventStore
+
+open Types
+open FableJson
+open System.IO
+
+type Msg<'Event> =
+  | GetAllEvents of AsyncReplyChannel<EventResult<'Event>>
+  | Events of EventSet<'Event>
+
+let store = @".\eventstore.json"
+
+let eventStore() : (unit -> EventResult<'Event>) * Subscriber<EventSet<'Event>> =
+  let mailbox =
+    MailboxProcessor.Start(fun inbox ->
+
+      let rec loop() =
+          async {
+            let! msg = inbox.Receive()
+
+            match msg with
+            | Events eventSet ->
+                printfn "EventStore received new events: %A" eventSet
+                try
+                  use streamWriter = new StreamWriter(store, true)
+
+                  eventSet
+                  |> toJson
+                  |> streamWriter.WriteLine
+
+                  do streamWriter.Flush()
+                with
+                | :? System.Exception as ex ->
+                    ex |> printfn "%A"
+
+                return! loop()
+
+            | GetAllEvents reply ->
+                try
+                  File.ReadAllLines(store)
+                  |> Array.map ofJson<EventSet<'Event>>
+                  |> Array.toList
+                  |> EventResult.Ok
+                  |> reply.Reply
+
+                with
+                | :? System.Exception as ex ->
+                    ex.Message
+                    |> EventResult.Error
+                    |> reply.Reply
+
+                return! loop()
+          }
+
+      loop()
+
+    )
+
+  let getAllEvents() =
+    mailbox.PostAndReply(GetAllEvents)
+
+  let eventSubscriber =
+    Events >> mailbox.Post
+
+  getAllEvents,eventSubscriber
+
+
+
--- src/Infrastructure/ReadHandler.fs
+++ src/Infrastructure/ReadHandler.fs
@@ -3,7 +3,7 @@ module Infrastructure.ReadHandler
 open Infrastructure.Types
 
 type Msg<'Event,'QueryParameter,'QueryResult> =
-  | Events of TransactionId*'Event list
+  | Events of EventSet<'Event>
   | Query of Query<'QueryParameter>*AsyncReplyChannel<QueryHandled<'QueryResult>>
 
 type State<'State> = 'State
@@ -11,13 +11,13 @@ type State<'State> = 'State
 let private createQueryHandler (handler : MailboxProcessor<Msg<'Event,'QueryParameter,'QueryResult>>) query =
     handler.PostAndReply(fun reply -> Msg.Query <| (query,reply))
 
-let private createReadmodel (handler : MailboxProcessor<Msg<'Event,'QueryParameter,'QueryResult>>) =
+let private createProjection (handler : MailboxProcessor<Msg<'Event,'QueryParameter,'QueryResult>>) =
   Msg.Events >> handler.Post
 
-let private readModelHandler (read : Readmodel<'State,'Event,'QueryParameter,'QueryResult>) : Projection<'Event> * QueryHandler<'QueryParameter,'QueryResult> =
+let private readHandler (read : Readmodel<'State,'Event,'QueryParameter,'QueryResult>) : Projection<'Event> * QueryHandler<'QueryParameter,'QueryResult> =
     let state = read.Projection.InitialState
 
-    let readProcessor =
+    let mailbox =
       MailboxProcessor.Start(fun inbox ->
 
         let rec loop state =
@@ -44,17 +44,16 @@ let private readModelHandler (read : Readmodel<'State,'Event,'QueryParameter,'Qu
         loop state
       )
 
-    let queryHandler =
-      createQueryHandler readProcessor
-
     let projection =
-      createReadmodel readProcessor
+      mailbox |> createProjection
+
+    let queryHandler =
+      mailbox |> createQueryHandler
 
     projection,queryHandler
 
 
-let initializeReadSide readmodels : Projection<'Event> list * (Query<'QueryParameter> -> QueryHandled<'QueryResult>) list =
+let initializeReadSide readmodels : Projection<'Event> list * QueryHandler<'QueryParameter,'QueryResult> list =
   readmodels
-    |> List.map readModelHandler
+    |> List.map readHandler
     |> List.unzip
-
